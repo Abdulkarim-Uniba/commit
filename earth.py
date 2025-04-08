ca19696fad78744e6fd72a5d8bc7e7bb2b7ca1cc
abdulkarim.zahidi <abdulkarim.zahidi.2004@gmail.com>
2025-04-08 18:31:30 +0200

    using ssh key

--- earth.py
+++ earth.py
@@ -1,3 +1,4 @@
 print("Hello earthling!")
 print("My name is Darth Vader, I am comming from a galaxt far far far away")
 print("I have the ability to use the force and swing lightsaber")
+print("I am a sith lord")
